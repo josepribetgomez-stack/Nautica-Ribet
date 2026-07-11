0bfb3ed8eb61554e7555e0bd47f27d4e4685bdc7
josepribetgomez-stack <josep.ribetgomez@gmail.com>
2026-07-11 11:33:22 +0100

    Create auth-config.js

---
+++ auth-config.js
@@ -0,0 +1,6 @@
+window.NAUTICA_SUPABASE_URL = 'https://uxgohiyyqrkktybgpxji.supabase.co';
+window.NAUTICA_SUPABASE_KEY = 'sb_publishable_FRJfiI4PSi-zDGv2krGWRw_EusX2jV4';
+window.nauticaSupabase = window.supabase.createClient(
+  window.NAUTICA_SUPABASE_URL,
+  window.NAUTICA_SUPABASE_KEY
+);
